a44a9e6b6bdd3acb134e9864d088f7d9dc576d2d
Maximo Bautista <max.bautista97@gmail.com>
2019-02-10 23:36:28 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -3,7 +3,7 @@ class CreateSongs < ActiveRecord::Migration[4.2]
     create_table :songs do |t| 
       t.string :name
       t.integer :genre_id
-      
+      t.integer :artist_id
     end
   end
 end
